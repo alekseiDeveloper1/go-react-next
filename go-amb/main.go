5cdb0b51c59e867d755769a17d7b74d1ac94eab7
Aleksey <Aleks_266@mail.ru>
2025-05-12 22:53:25 +0300

    add migrations

--- go-amb/main.go
+++ go-amb/main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-    "log"
+	"amb/src/database"
+	"log"
 
-    "github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3"
 )
 
 func main() {
-    _, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/amb"), &gorm.Open())
+	database.Connect()
+	database.AutoMigrate()
+	// Initialize a new Fiber app
+	app := fiber.New()
 
-    if err != nil {
-        panic("Could not connect with the database!")
-    }
-    // Initialize a new Fiber app
-    app := fiber.New()
+	// Define a route for the GET method on the root path '/'
+	app.Get("/", func(c fiber.Ctx) error {
+		// Send a string response to the client
+		return c.SendString("Hello, World 👋!")
+	})
 
-    // Define a route for the GET method on the root path '/'
-    app.Get("/", func(c fiber.Ctx) error {
-        // Send a string response to the client
-        return c.SendString("Hello, World 👋!")
-    })
-
-    // Start the server on port 3000
-    log.Fatal(app.Listen(":3000"))
+	// Start the server on port 3000
+	log.Fatal(app.Listen(":3000"))
 }
