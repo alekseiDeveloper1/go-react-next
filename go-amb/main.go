8ce9f2be93aa19db2854b19f887e32c67439862e
Aleksey <Aleks_266@mail.ru>
2025-05-15 08:24:53 +0300

    add air

--- go-amb/main.go
+++ go-amb/main.go
@@ -16,9 +16,9 @@ func main() {
 	// Define a route for the GET method on the root path '/'
 	app.Get("/", func(c fiber.Ctx) error {
 		// Send a string response to the client
-		return c.SendString("Hello, World 👋!")
+		return c.SendString("Hello, World 👋!sss")
 	})
 
 	// Start the server on port 3000
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":8000"))
 }
